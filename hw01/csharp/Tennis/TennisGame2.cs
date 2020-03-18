fcb4b972c22ce67dd23ae758b2d56dd6f0c03eff
Vojtech Bumba <456238@mail.muni.cz>
2020-03-18 17:20:16 +0100

    tennisGame2 round two

--- hw01/csharp/Tennis/TennisGame2.cs
+++ hw01/csharp/Tennis/TennisGame2.cs
@@ -16,6 +16,18 @@ namespace Tennis
             this.player2Name = player2Name;
         }
 
+        public string ChooseScore(int score)
+        {
+            switch (score)
+            {
+                case 0: return "Love";
+                case 1: return "Fifteen";
+                case 2: return "Thirty";
+                case 3: return "Forty";
+                default: return ""; // I would throw an exception here but that would be changing the logic
+            }
+        }
+
         public string GetScore()
         {
             var score = "";
@@ -30,54 +42,24 @@ namespace Tennis
                 return score.Length > 0 ? $"{score}-All" : "Deuce";
             }
 
-            if (p1point > 0 && p2point == 0)
+            if (p1point > 0 && p2point == 0 || p2point > 0 && p1point == 0)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > 0 && p1point == 0)
-            {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
-                score = p1res + "-" + p2res;
+                p1res = ChooseScore(p1point);
+                p2res = ChooseScore(p2point);
+                score = $"{p1res}-{p2res}";
             }
 
             if (p1point > p2point && p1point < 4)
             {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
+                p1res = ChooseScore(p1point);
+                p2res = ChooseScore(p2point);
+                score = $"{p1res}-{p2res}";
             }
             if (p2point > p1point && p2point < 4)
             {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
+                p1res = ChooseScore(p1point);
+                p2res = ChooseScore(p2point);
+                score = $"{p1res}-{p2res}";
             }
 
             if (p1point > p2point && p2point >= 3)
