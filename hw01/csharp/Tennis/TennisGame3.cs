afded22e135642844efa423ecb178f03e469c3fc
Vojtech Bumba <456238@mail.muni.cz>
2020-03-19 18:26:03 +0100

    tennisGame3

--- hw01/csharp/Tennis/TennisGame3.cs
+++ hw01/csharp/Tennis/TennisGame3.cs
@@ -2,41 +2,41 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int player2;
+        private int player1;
+        private string player1Name;
+        private string player2Name;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
         }
 
         public string GetScore()
         {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            string score;
+            if ((player1 < 4 && player2 < 4) && (player1 + player2 < 6))
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                string[] possible = { "Love", "Fifteen", "Thirty", "Forty" };
+                score = possible[player1];
+                return (player1 == player2) ? $"{score}-All" : $"{score}-{possible[player2]}";
             }
             else
             {
-                if (p1 == p2)
+                if (player1 == player2)
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                score = player1 > player2 ? player1Name : player2Name;
+                return ((player1 - player2) * (player1 - player2) == 1) ? $"Advantage {score}" : $"Win for {score}";
             }
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                this.p1 += 1;
-            else
-                this.p2 += 1;
+            if (playerName == player1Name)
+                this.player1++;
+            if (playerName == player2Name)
+                this.player2++;
         }
 
     }
